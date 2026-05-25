c52e1e3754a5ea8f73531a622f2b6432a7dda974
gusttavokr <gustavommilitaoo@gmail.com>
2026-05-25 09:28:06 -0300

    :lipstick: feat: estrelas

--- frontend/lib/core/widgets/background.dart
+++ frontend/lib/core/widgets/background.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class AppBackground extends StatelessWidget {
@@ -11,6 +12,7 @@ class AppBackground extends StatelessWidget {
           Container(
             color: const Color(0xFF0D0D1B),
           ),
+
           Container(
             decoration: const BoxDecoration(
               gradient: RadialGradient(
@@ -25,8 +27,96 @@ class AppBackground extends StatelessWidget {
               ),
             ),
           ),
+
+          const Positioned.fill(
+            child: Estrelas(),
+          ),
         ],
       ),
     );
+  }
+}
+
+class Estrelas extends StatefulWidget {
+  const Estrelas({super.key});
+
+  @override
+  State<Estrelas> createState() => EstrelasState();
+}
+
+class EstrelasState extends State<Estrelas> {
+
+  final List<StarData> stars = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    final random = Random();
+
+    for (int i = 0; i < 120; i++) {
+
+      stars.add(
+        StarData(
+          x: random.nextDouble(),
+          y: random.nextDouble(),
+          radius: 1,
+          opacity: 0.2,
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: StarsPainter(stars),
+    );
+  }
+}
+
+class StarData {
+  final double x;
+  final double y;
+  final double radius;
+  final double opacity;
+
+  StarData({
+    required this.x,
+    required this.y,
+    required this.radius,
+    required this.opacity,
+  });
+}
+
+class StarsPainter extends CustomPainter {
+
+  final List<StarData> stars;
+
+  StarsPainter(this.stars);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    final paint = Paint();
+
+    for (final star in stars) {
+
+      paint.color = Colors.white.withOpacity(star.opacity);
+
+      canvas.drawCircle(
+        Offset(
+          star.x * size.width,
+          star.y * size.height,
+        ),
+        star.radius,
+        paint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }

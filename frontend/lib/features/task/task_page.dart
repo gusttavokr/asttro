ed649134896a8d5d656f3b4f29ed5f72f7e24b2a
gusttavokr <gustavommilitaoo@gmail.com>
2026-05-29 14:34:24 -0300

    :sparkles: feat: animação na página de detalhe

--- frontend/lib/features/task/task_page.dart
+++ frontend/lib/features/task/task_page.dart
@@ -15,7 +15,52 @@ class TaskPage extends StatefulWidget {
   State<TaskPage> createState() => TaskPageState();
 }
 
-class TaskPageState extends State<TaskPage> {
+class TaskPageState extends State<TaskPage>
+    with SingleTickerProviderStateMixin {
+
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+  late Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 600,
+      ),
+    );
+
+    _fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeOut,
+      ),
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 0.15),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeOutCubic,
+      ),
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,8 +73,16 @@ class TaskPageState extends State<TaskPage> {
           left: 16,
           right: 16,
           bottom: 32,
+        ),
+        child: FadeTransition(
+          opacity: _fadeAnimation,
+          child: SlideTransition(
+            position: _slideAnimation,
+            child: Task(
+              task: widget.task,
+            ),
           ),
-        child: Task(task: widget.task),
+        ),
       ),
     );
   }
